d40bfe0a96086185933475479031e7879dbaa205
wanglei <64465800@qq.com>
2017-04-08 15:16:21 +0800

    edit README

---
+++ Index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/8
+ * Time: 15:06
+ */
+include_once "vendor/autoload.php";
+core\Bootstrap::run();
---
+++ core/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/8
+ * Time: 15:04
+ */
+namespace core;
+class Bootstrap{
+  public static function run(){
+    echo "welcome";
+  }
+}
