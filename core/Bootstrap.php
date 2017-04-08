13fb5a1b6ceebf31c181b91b98e5ea90120b6bd7
wanglei <64465800@qq.com>
2017-04-08 16:12:20 +0800

    edit README

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -13,6 +13,15 @@ class Bootstrap{
 //分析URL生成控制器方法常量
   public static function parseUrl()
   {
-    dd($_SERVER);
+    if(isset($_GET['s'])){
+      $info = explode('/',$_GET['s']);
+      $class = '\\web\\controller\\'.ucfirst($info[0]);
+      $action = $info[1];
+    }else{
+      $class = '\\web\\controller\\Index';
+      $action = 'show';
+    }
+    (new $class)->$action();
+    //$con->$action;
   }
 }
